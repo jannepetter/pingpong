656e147b67d81376cf7c076d044e05002d974c19
janne <peterdev80@gmail.com>
2020-08-13 13:19:39 +0300

    Updates

---
+++ precache-manifest.44fa1c023478f3e2e2daa00922dc4ff7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cacc832612a180b45ab35fb928f95d",
+    "url": "/jannepetter/pingpong.git/index.html"
+  },
+  {
+    "revision": "77ccc03de098cfeeb819",
+    "url": "/jannepetter/pingpong.git/static/css/main.ffb91199.chunk.css"
+  },
+  {
+    "revision": "8593f8aa9c56a736d52d",
+    "url": "/jannepetter/pingpong.git/static/js/2.96b9b089.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jannepetter/pingpong.git/static/js/2.96b9b089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ccc03de098cfeeb819",
+    "url": "/jannepetter/pingpong.git/static/js/main.07e5af43.chunk.js"
+  },
+  {
+    "revision": "8a94ff40a4d3a3948109",
+    "url": "/jannepetter/pingpong.git/static/js/runtime-main.fee6291f.js"
+  }
+]);
